43f268bec81cb5771d2bfb8a7029223fe9a19b99
田幸 <3269460536@qq.com>
2025-01-17 18:39:22 +0800

    部分页面试图实现（cyh）

---
+++ TelecommunicationsBigData-FrontEnd/src/views/SignalStrength/TypicalLandmarkSignalStrengthStatistics.vue
@@ -0,0 +1,111 @@
+<template lang="html">
+<el-form :inline="true" :model="formInline" class="demo-form-inline">
+    <el-form-item label="运营商">
+    <el-select
+        v-model="formInline.region"
+        placeholder=" "
+        clearable
+    >
+        <el-option label="CMCC" value="CMCC" />
+        <el-option label="CUCC" value="CUCC" />
+        <el-option label="CTCC" value="CTCC" />
+    </el-select>
+    </el-form-item>
+
+
+    <el-form-item>
+        <div class="demo-date-picker">
+            <div class="block">
+                <span class="demonstration" style="margin-right: 10px;">时间范围</span>
+                <el-date-picker
+                v-model="value2"
+                type="daterange"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+                :default-value="[new Date(2010, 9, 1), new Date(2010, 10, 1)]"
+                />
+            </div>
+        </div>
+
+    </el-form-item>
+
+    <el-form-item>
+    <el-button type="primary" @click="onSubmit">Query</el-button>
+    </el-form-item>
+</el-form>
+
+
+
+<div id="TypicalLandmarkSignalStrengthStatistics" style="background-color: #FFFFFF; height: 95%; width:95%; margin:.1rem; position: relative;">
+        <!-- <div style="position: absolute; top: 0; left: 0; font-size: 12px; color: black;">网络速率(下行)</div> -->
+    
+
+</div>
+    
+</template>
+
+<script setup>
+import * as echarts from 'echarts';
+import { onMounted } from "vue";
+
+function showImg() {
+    const myChart = echarts.init(document.getElementById('TypicalLandmarkSignalStrengthStatistics'));
+    const option = {
+        legend: {},
+        tooltip: {},
+        dataset: {
+            source: [
+                ['product', '4G', '3G', '2G'],
+                ['商业区', -100,-200,-300],
+                ['大学', -100,-200,-300],
+                ['其它', -100,-200,-300]
+            ]
+        },
+        xAxis: {
+            type: 'category',
+            axisLine: {
+                lineStyle: {
+                    color: 'blue' // 将 x 轴的线条颜色设置为蓝色
+                }
+            }
+        },
+        yAxis: {
+            axisLine: {
+                lineStyle: {
+                    color: 'blue' // 将 y 轴的线条颜色设置为蓝色
+                }
+            }
+        },
+        // Declare several bar series, each will be mapped
+        // to a column of dataset.source by default.
+        series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }]
+    };
+    myChart.setOption(option);
+}
+
+onMounted(() => {
+    showImg();
+})
+import { reactive } from 'vue'
+
+const formInline = reactive({
+    user: '',
+    region: '',
+    date: '',
+})
+
+const onSubmit = () => {
+console.log('submit!')
+}
+
+</script>
+
+<style>
+.demo-form-inline .el-input {
+--el-input-width: 150px;
+}
+
+.demo-form-inline .el-select {
+--el-select-width: 150px;
+}
+</style>
---
+++ TelecommunicationsBigData-FrontEnd/src/views/SignalStrength/TypicalLandmarkSignalStrengthTracking.vue
@@ -0,0 +1,309 @@
+<template lang="html">
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+        <el-form-item label="地标">
+      <el-select
+        v-model="formInline.region"
+        placeholder=" "
+        clearable
+      >
+        <el-option label="大学" value="大学" />
+        <el-option label="居民区" value="居民区" />
+        <el-option label="商业区" value="商业区" />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="网络制式">
+      <el-select
+        v-model="formInline.region"
+        placeholder=" "
+        clearable
+      >
+        <el-option label="2G" value="2G" />
+        <el-option label="3G" value="3G" />
+        <el-option label="4G" value="4G" />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="时间粒度">
+      <el-select
+        v-model="formInline.region"
+        placeholder=" "
+        clearable
+      >
+        <el-option label="月" value="月" />
+        <el-option label="天" value="天" />
+        <el-option label="时" value="时" />
+      </el-select>
+    </el-form-item>
+
+    <el-form-item>
+        <div class="demo-date-picker">
+            <div class="block">
+            <span class="demonstration" style="margin-right: 10px;">时间范围</span>
+            <el-date-picker
+                v-model="value2"
+                type="daterange"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+                :default-value="[new Date(2010, 9, 1), new Date(2010, 10, 1)]"
+            />
+            </div>
+        </div>
+
+    </el-form-item>
+
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">Query</el-button>
+    </el-form-item>
+  </el-form>
+    <div id="typicalLandmarkSignalStrengthTracking" style="background-color: #FFFFFF; height: 95%; width:95%; margin:.1rem; position: relative;"></div>
+    
+
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const value1 = ref('')
+const value2 = ref('')
+import * as echarts from 'echarts';
+import { onMounted } from "vue";
+
+function showImg() {
+    const myChart = echarts.init(document.getElementById('typicalLandmarkSignalStrengthTracking'));
+    const colors = ['#5470C6', '#EE6666', '#90EE90', '#FFA500', '#87CEFA']; // 曲线颜色保持不变
+    const option = {
+        color: colors,
+        tooltip: {
+            trigger: 'none',
+            axisPointer: {
+                type: 'cross'
+            }
+        },
+        legend: {
+            data: ['CMCC', 'CUCC', 'CTCC']
+        },
+        grid: {
+            top: 50,
+            bottom: 80,
+            left: 80,   // 扩大图表的显示区域
+            right: 80,  // 扩大图表的显示区域
+        },
+        xAxis: [
+            {
+                type: 'category',
+                axisTick: {
+                    alignWithLabel: true
+                },
+                axisLine: {
+                    onZero: false,
+                    lineStyle: {
+                        color: 'red' // 横坐标轴线颜色改为红色
+                    }
+                },
+                axisPointer: {
+                    label: {
+                        formatter: function (params) {
+                            return (
+                                'Precipitation  ' +
+                                params.value +
+                                (params.seriesData.length? '：' + params.seriesData[0].data : '')
+                            );
+                        }
+                    }
+                },
+                data: Array.from({length: 30}, (_, i) => (i + 1).toString().padStart(2, '0')),
+                position: 'top',
+                axisLabel: {
+                    interval: 0,
+                    formatter: function (value) {
+                        return `${value}`;
+                    },
+                    textStyle: {
+                        color: 'red' // 横坐标标签颜色改为红色
+                    }
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value',
+                min: -120,
+                max: 0,
+        interval: 30,
+                axisLabel: {
+                    formatter: '{value}',
+                    textStyle: {
+                        color: 'red' // 纵坐标标签颜色改为红色
+                    }
+                },
+                splitLine: {  // 调整竖线的颜色
+                    lineStyle: {
+                        color: '#B0B0B0', // 竖线颜色淡一点
+                    }
+                },
+                axisLine: {
+                    lineStyle: {
+                        color: 'red' // 纵坐标轴线颜色改为红色
+                    }
+                }
+            }
+        ],
+        series: [
+            {
+                name: 'CMCC',
+                type: 'line',
+                xAxisIndex: 0,
+                smooth: true,
+                emphasis: {
+                    focus: 'series'
+                },
+                symbol: 'circle', // 使用圆形表示 CMCC
+                symbolSize: 8, // 增大标记大小
+                clipPath: {
+                    type: 'rect',
+                    x: '5%',
+                    y: '15%',
+                    width: '90%',
+                    height: '80%'
+                },
+                data: [
+                    -30, -60, -90, -120, -90, -60, -30, 0, -30, -60, -90, -120, -90, -60, -30, 0, -30, -60, -90, -120, -90, -60, -30, 0, -30, -60, -90, -120, -90, -60
+                ],
+                markLine: {
+                    symbol: 'none', // 去掉箭头
+                    symbolSize: [0, 0], // 去掉箭头尺寸
+                    data: Array.from({length: 30}, (_, i) => ({
+                        xAxis: (i + 1).toString().padStart(2, '0'),
+                        lineStyle: {
+                            color: '#ccc',
+                            type: 'solid',
+                            width: 1
+                        },
+                        label: { show: false }
+                    }))
+                }
+            },
+            {
+                name: 'CUCC',
+                type: 'line',
+                xAxisIndex: 0,
+                smooth: true,
+                emphasis: {
+                    focus: 'series'
+                },
+                symbol: 'rect', // 使用正方形表示 CUCC
+                symbolSize: 8, // 增大标记大小
+                clipPath: {
+                    type: 'rect',
+                    x: '5%',
+                    y: '15%',
+                    width: '90%',
+                    height: '80%'
+                },
+                data: [
+                    -20, -50, -80, -110, -80, -50, -20, 0, -20, -50, -80, -110, -80, -50, -20, 0, -20, -50, -80, -110, -80, -50, -20, 0, -20, -50, -80, -110, -80, -50
+                ],
+                markLine: {
+                    symbol: 'none', // 去掉箭头
+                    symbolSize: [0, 0], // 去掉箭头尺寸
+                    data: Array.from({length: 30}, (_, i) => ({
+                        xAxis: (i + 1).toString().padStart(2, '0'),
+                        lineStyle: {
+                            color: '#ccc',
+                            type: 'solid',
+                            width: 1
+                        },
+                        label: { show: false }
+                    }))
+                }
+            },
+            {
+                name: 'CTCC',
+                type: 'line',
+                xAxisIndex: 0,
+                smooth: true,
+                emphasis: {
+                    focus: 'series'
+                },
+                symbol: 'triangle', // 使用三角形表示 CTCC
+                symbolSize: 8, // 增大标记大小
+                clipPath: {
+                    type: 'rect',
+                    x: '5%',
+                    y: '15%',
+                    width: '90%',
+                    height: '80%'
+                },
+                data: [
+                    -40, -70, -100, -110, -70, -40, -10, 0, -40, -70, -100, -110, -70, -40, -10, 0, -40, -70, -100, -110, -70, -40, -10, 0, -40, -70, -100, -110, -70, -40
+                ],
+                markLine: {
+                    symbol: 'none', // 去掉箭头
+                    symbolSize: [0, 0], // 去掉箭头尺寸
+                    data: Array.from({length: 30}, (_, i) => ({
+                        xAxis: (i + 1).toString().padStart(2, '0'),
+                        lineStyle: {
+                            color: '#ccc',
+                            type: 'solid',
+                            width: 1
+                        },
+                        label: { show: false }
+                    }))
+                }
+            }
+        ],
+        graphic: [
+            {
+                type: 'text',
+                left: '5%',
+                top: '1%',
+                style: {
+                    text: '典型地表信号强度跟踪图   单位(dBm)',
+                    fontSize: 18,
+                    fontWeight: 'bold'
+                }
+            }
+        ]
+    };
+
+    // // 调整图的比例
+    // option.grid.containLabel = true;
+
+    myChart.setOption(option);
+}
+
+onMounted(() => {
+    showImg();
+})
+
+
+import { reactive } from 'vue'
+
+const formInline = reactive({
+  user: '',
+  region: '',
+  date: '',
+})
+
+const onSubmit = () => {
+  console.log('submit!')
+}
+</script>
+
+<style lang="css">
+.demo-form-inline .el-input {
+  --el-input-width: 150px;
+}
+
+.demo-form-inline .el-select {
+  --el-select-width: 150px;
+}
+.demo-date-picker.block {
+  border-right: solid 1px var(--el-border-color);
+  flex: 1;
+}
+.demo-date-picker.demonstration {
+  color: var(--el-text-color-secondary);
+  font-size: 14px;
+}
+</style>
