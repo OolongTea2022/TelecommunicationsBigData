92975ded180096de58091aa7d0f8aeb70bd7bbd4
Ptfty <2310957061@qq.com>
2025-01-17 16:13:53 +0800

    业务7到13基本完成

---
+++ TelecommunicationsBigData-BackEnd/src/main/java/com/bigdata/service/impl/NwQualityServiceImpl.java
@@ -0,0 +1,58 @@
+package com.bigdata.service.impl;
+
+import com.bigdata.VO.NetworkQuality.NwQualityStatisticsVo;
+import com.bigdata.VO.NetworkQuality.NwSpeedRankVo;
+import com.bigdata.VO.NetworkQuality.TypicalNWQualityTrackingVo;
+import com.bigdata.dao.NwQualityMapper;
+import com.bigdata.dto.nwQuality.NwTrackingDTO;
+import com.bigdata.dto.nwQuality.QualityStatisticsDTO;
+import com.bigdata.dto.nwQuality.SpeedRankDTO;
+import com.bigdata.model.entity.NetworkQuality.NwQualityStatistics;
+import com.bigdata.model.entity.NetworkQuality.NwSpeedRank;
+import com.bigdata.model.entity.NetworkQuality.TypicalNWQualityTracking;
+import com.bigdata.service.NwQualityService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class NwQualityServiceImpl implements NwQualityService {
+
+    @Autowired
+    private NwQualityMapper nwQualityMapper;
+
+    @Override
+    public List<NwQualityStatisticsVo> getQualityStatistics(QualityStatisticsDTO statisticsDTO) {
+
+        List<NwQualityStatistics> nwQualityStatistics =  nwQualityMapper.getStatistics(statisticsDTO);
+        List<NwQualityStatisticsVo> result = nwQualityStatistics.parallelStream()
+                .map(item ->{
+                    NwQualityStatisticsVo vo = new NwQualityStatisticsVo();
+                    BeanUtils.copyProperties(item, vo);
+                    return vo;
+                }).collect(Collectors.toList());
+        return result;
+    }
+
+    @Override
+    public List<NwSpeedRankVo> getRank(SpeedRankDTO speedRankDTO) {
+        List<NwSpeedRank> ranks = nwQualityMapper.getRanks(speedRankDTO);
+        List<NwSpeedRankVo> result = ranks.parallelStream()
+                .map(item->{
+                    NwSpeedRankVo vo = new NwSpeedRankVo();
+                    BeanUtils.copyProperties(item, vo);
+                    return vo;
+                }).collect(Collectors.toList());
+        return result;
+    }
+
+    @Override
+    public List<TypicalNWQualityTrackingVo> getTypicalTracking(NwTrackingDTO trackingDTO) {
+        List<TypicalNWQualityTracking> trackings = nwQualityMapper.getTypicalTracking(trackingDTO);
+        return Collections.emptyList();
+    }
+}
