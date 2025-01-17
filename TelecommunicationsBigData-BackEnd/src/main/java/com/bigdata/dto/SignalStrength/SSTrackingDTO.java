fb672b81bcace05a4def2b1cd76d23ed93a5a001
lENOVO <3151629445@qq.com>
2025-01-17 21:47:38 +0800

    添加文档注解

--- TelecommunicationsBigData-BackEnd/src/main/java/com/bigdata/dto/SignalStrength/SSTrackingDTO.java
+++ TelecommunicationsBigData-BackEnd/src/main/java/com/bigdata/dto/SignalStrength/SSTrackingDTO.java
@@ -1,14 +1,20 @@
 package com.bigdata.dto.SignalStrength;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class SSTrackingDTO {
+    @Schema(description = "地标",example = "commercial")
     private String landmark;
+    @Schema(description = "网络类型",example = "2G")
     private String networkType;
+    @Schema(description = "开始日期",example = "2019090517")
     private long startDate;
+    @Schema(description = "截止日期",example = "2019090517")
     private long endDate;
+    @Schema(description = "时间粒度",example = "day,hour,month")
     private String timeGranularity;
 }
