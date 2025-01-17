0e67647a2de9e70169517e2332a6b7abb4abecea
OolongTea <jiangbx0422@126.com>
2025-01-17 18:12:47 +0800

    地图部分尝试

--- TelecommunicationsBigData-FrontEnd/src/views/SignalStrength/SignalStrengthDistribution.vue
+++ TelecommunicationsBigData-FrontEnd/src/views/SignalStrength/SignalStrengthDistribution.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
-        <div id="container" ref="dom"></div>
-        <div id="main" style="height: 700px; width: 100%;"></div>
+        <div id="main" style="height: 700px; width: 80%;"></div>
     </div>
 </template>
 
@@ -9,71 +8,70 @@
 <script setup>
 
     import * as echarts from 'echarts';
-import 'echarts/extension/bmap/bmap';
-import { onMounted, ref } from 'vue';
+    import 'echarts/extension/bmap/bmap';
+    import { onMounted , ref} from 'vue';
 
-    const dom = ref();
-    let map;
-    let myChart;
 
-    onMounted(() => {
-        map = new window.BMapGL.Map(dom.value);
-        var point = new window.BMapGL.Point(116.404, 39.915);
-        map.centerAndZoom(point, 15);
-        map.enableScrollWheelZoom(true);
-        
-        var chartDom = document.getElementById('main');
-        myChart = echarts.init(chartDom);
-        
-        var option;
-        fetch('https://echarts.apache.org/examples/data/asset/data/hangzhou-tracks.json')
-            .then(response => response.json())
-            .then(data => {
-                var points = [].concat.apply(
-                    [],
-                    data.map(function (track) {
-                        return track.map(function (seg) {
-                            return seg.coord.concat([1]);
-                        });
-                    })
-                );
-                myChart.setOption(
-                    (option = {
-                        animation: false,
-                        bmap: {
-                            center: [116.404, 39.915],
-                            zoom: 15,
-                            roam: true
-                        },
-                        visualMap: {
-                            show: false,
-                            top: 'top',
-                            min: 0,
-                            max: 5,
-                            seriesIndex: 0,
-                            calculable: true,
-                            inRange: {
-                                color: ['blue', 'blue', 'green', 'yellow', 'red']
-                            }
-                        },
-                        series: [
-                            {
-                                type: 'heatmap',
-                                coordinateSystem: 'bmap',
-                                data: points,
-                                pointSize: 5,
-                                blurSize: 6
-                            }
-                        ]
-                    })
-                );
-                var bmap = myChart.getModel().getComponent('bmap').getBMap();
-                bmap.addControl(new BMap.MapTypeControl());
-            })
-            .catch(error => console.error('Error fetching data:', error));
+function showImg(){
 
-        option && myChart.setOption(option);
-    })
+    const chartDom = document.getElementById('main');
+    const myChart = echarts.init(chartDom);
+
+    const points = ref([
+        [116.404, 39.915, 1],
+        [116.404, 39.9151, 1],
+        [116.404, 39.9152, 1],
+        [116.404, 39.9153, 1],
+        [116.404, 39.9154, 1],
+        [116.404, 39.9155, 1],
+        [116.404, 39.9156, 1],
+        [116.404, 39.9157, 1],
+        [116.404, 39.9158, 1],
+        [116.404, 39.9159, 10]
+    ]);
+
+    const option = {
+        animation: false,
+        bmap: {
+            center: [116.404, 39.915],
+            zoom: 17,
+            roam: true
+        },
+        visualMap: {
+            show: false,
+            top: 'top',
+            min: 0,
+            max: 5,
+            seriesIndex: 0,
+            calculable: true,
+            inRange: {
+                color: ['blue', 'blue', 'green', 'yellow', 'red', 'purple', '']
+            }
+        },
+        series: [
+            {
+                type: 'heatmap',
+                coordinateSystem: 'bmap',
+                data: points.value,
+                pointSize: 10,
+                blurSize: 1
+            }
+        ]
+    }
+
+    myChart.setOption(option);
+
+    // 添加百度地图插件
+    var bmap = myChart.getModel().getComponent('bmap').getBMap();
+    bmap.addControl(new BMap.MapTypeControl());
+
+
+}
+
+
+onMounted(()=>{
+showImg();
+})
 
 </script>
 
