7d7e57f4d9e14ed7d127d37fbb3c1b2309ab6309
OolongTea <jiangbx0422@126.com>
2025-01-17 22:11:59 +0800

    添加请求函数文件

--- TelecommunicationsBigData-FrontEnd/src/utils/request.js
+++ TelecommunicationsBigData-FrontEnd/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const myAxios = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "http://localhost:8081",
     timeout: 10000,
     withCredentials: true,
 });
